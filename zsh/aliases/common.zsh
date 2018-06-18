2320402f60a9fa2c98b82ad6e63bfa5a3b7e73b7
Vicente Pons <vicentepons.rb@gmail.com>
2018-06-18 12:52:55 +0200

    Add zsh configuration

---
+++ zsh/aliases/common.zsh
@@ -0,0 +1,12 @@
+# Common aliases
+
+alias ls='ls --color=auto'
+alias la='ls -lAFh'
+alias ll='ls -l'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias dc='docker-compose'
+alias dcr='dc run --rm'

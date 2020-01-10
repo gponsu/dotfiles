a034acba90ee7d2474c6e9e151f00e287031af1e
gponsu <simpons@gmail.com>
2020-01-10 16:46:29 +0100

    Remove tmux support

--- install.sh
+++ install.sh
@@ -10,8 +10,6 @@ INSTALL_DIR=$HOME/.dotfiles
 echo "${colors[cyan]}=> Installing plugin managers...${colors[default]}"
 command curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 command curl -fLo ~/.antigen/antigen.zsh --create-dirs git.io/antigen
-command git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
 
 echo "${colors[cyan]}=> Downloading dotfiles from git to '$INSTALL_DIR'...${colors[default]}"
 command git -c advice.detachedHead=false clone --recursive \
@@ -19,7 +17,6 @@ command git -c advice.detachedHead=false clone --recursive \
 
 echo "${colors[cyan]}=> Creating symbolic links to configuration files...${colors[default]}"
 command ln -vsf $INSTALL_DIR/vimrc.conf $HOME/.vimrc
-command ln -vsf $INSTALL_DIR/tmux.conf $HOME/.tmux.conf
 command ln -vsf $INSTALL_DIR/zshrc.conf $HOME/.zshrc
 
 echo "${colors[green]}=> ✔ Completed, enjoy!${colors[default]}"

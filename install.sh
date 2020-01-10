47cf73f03e39a61e4cb1dd4972997254dea6a999
gponsu <simpons@gmail.com>
2020-01-10 16:49:57 +0100

    Replace vim by neovim

--- install.sh
+++ install.sh
@@ -5,18 +5,19 @@ declare -A colors=(
   ["green"]=`tput setaf 2`
   ["cyan"]=`tput setaf 6`)
 
-INSTALL_DIR=$HOME/.dotfiles
+INSTALL_DIR=$HOME/.local/share/dotfiles
 
 echo "${colors[cyan]}=> Installing plugin managers...${colors[default]}"
-command curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 command curl -fLo ~/.antigen/antigen.zsh --create-dirs git.io/antigen
+command curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "${colors[cyan]}=> Downloading dotfiles from git to '$INSTALL_DIR'...${colors[default]}"
 command git -c advice.detachedHead=false clone --recursive \
         https://github.com/gponsu/dotfiles.git --depth=1 "${INSTALL_DIR}"
 
 echo "${colors[cyan]}=> Creating symbolic links to configuration files...${colors[default]}"
-command ln -vsf $INSTALL_DIR/vimrc.conf $HOME/.vimrc
+command ln -vsf $INSTALL_DIR/vimrc.conf $HOME/.config/nvim/init.vim
 command ln -vsf $INSTALL_DIR/zshrc.conf $HOME/.zshrc
 
 echo "${colors[green]}=> ✔ Completed, enjoy!${colors[default]}"
